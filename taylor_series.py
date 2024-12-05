bb70ec010a4294c46b0c690ade3331142053bf74
LaLa4978 <159722251+LaLa4978@users.noreply.github.com>
2024-12-05 22:55:15 +0900

    Update taylor_series.py

--- taylor_series.py
+++ taylor_series.py
@@ -3,8 +3,11 @@ import sympy as sp
 def taylor_series():
     formula_input = input("함수 입력: ")
     x = sp.Symbol('x')
-    formula = sp.sympify(formula_input)
+    formula = sp.sympify(formula_input
 
+    center = float(input("급수의 중심 입력: "))
+    order = int(input("급수의 차수 입력: "))
 
-    taylor = sp.series(formula, x, 0, 6)
+    
+    taylor = sp.series(formula, x, center, order + 1).remove0()
     print("테일러 급수: {}". format(taylor))
