b51b45fe613d7515e64a5f9f13e930a531bf162a
F1R3FIY <jewon2013@naver.com>
2024-12-06 18:43:07 +0900

    #4 issue retouch

--- taylor-seriesgap.py
+++ taylor-seriesgap.py
@@ -8,7 +8,13 @@ class TaylorSeriesPlotter:
         self.variable = sp.Symbol(variable)
 
     def taylor_series(self, center, order):
-        return sp.series(self.function, self.variable, center, order + 1).removeO()
+        """
+        Sympy의 series 메서드가 지정된 차수보다 하나 높은 항까지 계산해서
+        결과가 예상과 다르게 보일 수도 있습니다. 출력된 급수로 비교해 보세요. 일단 조금 손봤습니다
+        """
+        taylor_expr = sp.series(self.function, self.variable, center, order + 1).removeO()
+        print(f"Taylor Series Expansion (Order {order}): {taylor_expr}")
+        return taylor_expr
 
     def plot_comparison(self, center, orders, x_range=(-10, 10), num_points=500):
         x_vals = np.linspace(x_range[0], x_range[1], num_points)
@@ -40,10 +46,11 @@ class TaylorSeriesPlotter:
 
         fig.show()
 
+
 if __name__ == "__main__":
-    function_input = input("function (ex: sin(x), exp(x), log(1+x)): ")
-    center_input = float(input("the center of the series: "))
-    orders_input = input("degree (ex: 2,4,6): ")
+    function_input = input("function(e.g., sin(x), exp(x), log(1+x)): ")
+    center_input = float(input("center: "))
+    orders_input = input("degrees(e.g., 2,4,6): ")
     orders = list(map(int, orders_input.split(',')))
 
     plotter = TaylorSeriesPlotter(function_input)
