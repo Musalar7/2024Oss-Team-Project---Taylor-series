5d3c77cc41f2bbd686b6725fc94a43c91ffe86f8
Jinkun Koh <kjk0719@hanyang.ac.kr>
2024-12-06 14:35:58 +0900

    Taylor_Series-graph_diff_fixed_1.py

--- Taylor_Series-graph_diff.py
+++ Taylor_Series-graph_diff.py
@@ -1,30 +1,23 @@
-        diff = y_original - y_taylor
-        diff_area = 0.5
-        
-        for i in range(len(diff)):
-            if diff[i] > 0:
-                fig.add_trace(go.Scatter(
-                    x=[x_vals[i]], y=[y_original[i]],
-                    mode= "markers",
-                    color = "blue", size = 4
-                ))
+# Class 내부, 오차 표시
+def diff_area(self, x_vals, y_original, y_taylor):
+        error = y_original - y_taylor
+        area = []
+
+        for i in range(1, len(x_vals)):  
+            x_area = [x_vals[i-1], x_vals[i], x_vals[i], x_vals[i-1]]
+            y_area = [y_taylor[i-1], y_taylor[i], y_original[i], y_original[i-1]]
+
+            
+            if error[i] > 0:
+                color = "rgba(0, 0, 255, 0.2)"
             else:
-                fig.add_trace(go.Scatter(
-                    x=[x_vals[i]], y=[y_original[i]],
-                    mode="markers",
-                    color = "red", size = 4
-                ))
+                color = "rgba(255, 0, 0, 0.2)"
 
-        
-        
-        fig2 = go.Figure()
-        fig2.add_trace(go.Scatter(x=x_vals, y=error, mode='lines', name="Diff Graph"))
+            area.append(go.Scatter(x=x_area, y=y_area, fill='toself', mode='lines', line_width=0, fillcolor = color, showlegend = False))
 
-        fig2.update_layout(
-            title="Diff Graph",
-            xaxis_title="x",
-            yaxis_title="Difference",
-        )
+        return area
 
-        
-        fig2.show()             # fig.show() 와 함께 그래프를 표시
+# for order in orders: 구문 안 제일 밑에 삽입
+area = self.diff_area(x_vals, y_original, y_taylor)
+            for diff in area:
+                fig.add_trace(diff)
