eb7e0329e401534007d425cf32a22d6d7cfd06b7
LaLa4978 <159722251+LaLa4978@users.noreply.github.com>
2024-12-08 16:17:43 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 language = 'ko'
 templates_path = ['_templates']
