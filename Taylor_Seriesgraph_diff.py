d7552bed74df47f7f296b1a3af7ed2e7360a934f
LaLa4978 <159722251+LaLa4978@users.noreply.github.com>
2024-12-09 19:32:27 +0900

    Update Taylor_Seriesgraph_diff.py

--- Taylor_Seriesgraph_diff.py
+++ Taylor_Seriesgraph_diff.py
@@ -1,25 +1,27 @@
 # Class 내부, 오차 표시
 def diff_area(self, x_vals, y_original, y_taylor):
-        error = y_original - y_taylor
-        area = []
-
-        for i in range(1, len(x_vals)):  
-            x_area = [x_vals[i-1], x_vals[i], x_vals[i], x_vals[i-1]]
-            y_area = [y_taylor[i-1], y_taylor[i], y_original[i], y_original[i-1]]
-
-            
-            if error[i] > 0:
-                color = "rgba(0, 0, 255, 0.2)"
-            else:
-                color = "rgba(255, 0, 0, 0.2)"
-
-            area.append(go.Scatter(x=x_area, y=y_area, fill='toself', mode='lines', line_width=0, fillcolor = color, showlegend = False))
-
-        return area
+        """
+        
+        :param x_vals: 함수 그래프의 x값 목록
+        :param y_original: 원본 함수의 y값
+        :param y_taylor: 테일러 급수의 y값
+              
+        :return: 차이 영역을 나타내는 **Plotly 그래프 객체** 목록
 
 
-# 함수 호출 방법
-# for order in orders: 구문 안 제일 밑에 삽입
-# area = self.diff_area(x_vals, y_original, y_taylor)
-            # for diff in area:
-                # fig.add_trace(diff)
+        **작동 방식**:
+           - 함수는 각 `x` 값에 대해, 원본 함수와 테일러 급수 간의 차이(오차)를 계산합니다.
+           - `y_original - y_taylor` 값이 양수일 경우 해당 구간을 파란색으로, 음수일 경우 빨간색으로 채웁니다.
+           - 이 차이를 그래프 영역으로 강조하고, 이를 Plotly Scatter 객체로 반환하여 `plot_comparison`에서 시각적으로 추가합니다.
+
+        
+        **작동 예시**:
+           x_vals = np.linspace(-10, 10, 500)
+           y_original = np.sin(x_vals)
+           y_taylor = np.taylor_series(x_vals, center=0, order=2)
+
+           area = diff_area(x_vals, y_original, y_taylor)
+           for diff in area:
+               fig.add_trace(diff)
+               
+           """
