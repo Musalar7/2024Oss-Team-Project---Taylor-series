cf8b1dd1e85eecb7b5d033565a9f28ddfe34ce4c
LaLa4978 <159722251+LaLa4978@users.noreply.github.com>
2024-12-09 18:11:53 +0900

    Update taylor_series.py

--- taylor_series.py
+++ taylor_series.py
@@ -2,11 +2,7 @@
 __version__ = "0.1.0"
 
 
-class InvalidKindError(Exception):
-    """Raised if the kind is invalid."""
-    pass
-
-def taylor_series(kind=None):
+def taylor_series():
     """
     
     :param function: 수학적 표현으로 입력된 함수
@@ -20,6 +16,8 @@ def taylor_series(kind=None):
     :rtype: None
 
     예시:
+    
     1. `taylor_series()` 실행 후 함수로 `sin(x)` 입력
+    
     2. 중심을 `0`으로 입력하고, 차수 `[2, 4, 6]` 입력 시 `sin(x)`의 테일러 급수 2차, 4차, 6차 근사값이 시각적으로 표시됩니다.
     """
