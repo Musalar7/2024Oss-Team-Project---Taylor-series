11f4945c1d6025cd675aa100f329b3f192526aa7
LaLa4978 <159722251+LaLa4978@users.noreply.github.com>
2024-12-09 18:04:20 +0900

    Update taylor_series.py

--- taylor_series.py
+++ taylor_series.py
@@ -1,6 +1,3 @@
-"""
-Lumache - Python library for cooks and food lovers.
-"""
 
 __version__ = "0.1.0"
 
@@ -11,17 +8,13 @@ class InvalidKindError(Exception):
 
 def get_random_ingredients(kind=None):
     """
-    :param function: 수학적 표현으로 입력된 함수
-    :type function: str
-    :param center: 테일러 급수의 전개 중심
-    :type center: int
-    :param orders: 테일러 급수의 차수 목록
-    :type orders: list[int]
+    Return a list of random ingredients as strings.
 
-    :return: None
-    :rtype: None
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
 
-    예시:
-    1. `taylor_series()` 실행 후 함수로 `sin(x)` 입력
-    2. 중심을 `0`으로 입력하고, 차수 `[2, 4, 6]` 입력 시 `sin(x)`의 테일러 급수 2차, 4차, 6차 근사값이 시각적으로 표시됩니다.
     """
+    return ["shells", "gorgonzola", "parsley"]
